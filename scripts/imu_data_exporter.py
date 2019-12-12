d7d998c4e6e15f701cc579145ae65f34d330dc89
midasama3124 <miguel.sanchez-m@mail.escuelaing.edu.co>
2019-12-12 11:47:33 -0500

    All functionalities described in the documentation are working.

--- scripts/imu_data_exporter.py
+++ scripts/imu_data_exporter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import rospy
 import rospkg
-from exo_msgs.msg import IMUData
+from sensor_msgs.msg import Imu
 import time
 import datetime
 from os.path import expanduser
@@ -9,13 +9,13 @@ from os.path import expanduser
 class IMU_Data_Exporter(object):
     def __init__(self):
         rospy.init_node("export_imu_data",anonymous=True)
-        rospy.Subscriber("/imu_data", IMUData, self.callback)
+        rospy.Subscriber("/imu_data", Imu, self.callback)
         self.start_time = time.time()
         self.patient = rospy.get_param("gait_phase_det/patient")
         # Reading and logging imu_data
         now = datetime.datetime.now()
         rospack = rospkg.RosPack()
-        packpath = rospack.get_path('exo_gait_phase_det')
+        packpath = rospack.get_path('hmm_gait_phase_classifier')
         self.f = open(packpath + '/log/IMU_data/' + self.patient + '_'+str(now.month)+'-'+str(now.day)+'-'+str(now.hour)+'-'+str(now.minute)+'-'+str(now.second)+'-'+'.csv','wb')
         self.f.write('time_stamp,gyro_x,gyro_y,gyro_z,accel_x,accel_y,accel_z,quat_x,quat_y,quat_z,quat_w\n')
         # self.f.write('0,0,0,0,0,0,0\n')
@@ -26,7 +26,7 @@ class IMU_Data_Exporter(object):
         rospy.loginfo("Time: {} seconds".format( time_stamp/1000.0 ))
 
         # Reading and logging imu_data
-        self.f.write('{},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}\n'.format(time_stamp,msg.gyro_x,msg.gyro_y,msg.gyro_z,msg.accel_x,msg.accel_y,msg.accel_z,msg.quat_x,msg.quat_y,msg.quat_z,msg.quat_w))
+        self.f.write('{},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}\n'.format(time_stamp,msg.angular_velocity.x,msg.angular_velocity.y,msg.angular_velocity.z,msg.linear_acceleration.x,msg.linear_acceleration.y,msg.linear_acceleration.z,msg.orientation.x,msg.orientation.y,msg.orientation.z,msg.orientation.w))
 
 if __name__ == "__main__":
     time.sleep(5)
